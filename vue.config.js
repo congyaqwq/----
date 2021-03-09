bd16c233a68a8f75361662eeef0753c819a4cc02
李聪 <lc182540@163.com>
2021-03-09 17:58:30 +0800

    fix:修改地址

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    publicPath: isProd ? '/backend/' : '/',
+    publicPath: isProd ? '/admin/' : '/',
     css: {
         loaderOptions: {
             less: {

e270cedadf80c360f4e8e585bf3475672d4b9c76
cong <lc182540@163.com>
2021-11-29 16:01:01 +0800

    fix: proxy

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,21 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-    publicPath: isProd ? '/admin/' : '/',
-    css: {
-        loaderOptions: {
-            less: {
-                javascriptEnabled: true
-            }
-        }
-    }
-}
+  publicPath: isProd ? "/admin/" : "/",
+  css: {
+    loaderOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:3000",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+};
